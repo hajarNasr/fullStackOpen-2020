4f6e3c233f47fea4cd9da0f5e07329fd332690fb
hajar nasr <hajar-nasr@hotmail.com>
2021-01-24 15:11:38 +0200

    fix: define Button outside App

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Button = ({ handleClick, text }) => (
+  <button
+    onClick={handleClick}
+    style={{
+      marginLeft: "5px",
+      backgroundColor: "teal",
+      color: "white",
+      border: "none",
+      boxShadow: "1px 1px 1px rgba(0,0,0,0.3)",
+      borderRadius: "3px",
+    }}
+  >
+    {text}
+  </button>
+);
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const Button = ({ handleClick, text }) => (
-    <button
-      onClick={handleClick}
-      style={{
-        marginLeft: "5px",
-        backgroundColor: "teal",
-        color: "white",
-        border: "none",
-        boxShadow: "1px 1px 1px rgba(0,0,0,0.3)",
-        borderRadius: "3px",
-      }}
-    >
-      {text}
-    </button>
-  );
   const giveGood = () => setGood(good + 1);
   const giveNeutral = () => setNeutral(neutral + 1);
   const giveBad = () => setBad(bad + 1);
